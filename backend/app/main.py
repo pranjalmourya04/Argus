293919f207ebde2d9115482a6732119181e455ea
Pranjal Mourya <pranjalmourya04@gmail.com>
2026-03-12 23:14:25 +0530

    Update CORS allowed origins for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
     "http://localhost:3000",
-    "https://argus-ai-ten.vercel.app"
+    "https://argus-ai-tau.vercel.app"
 ],
     allow_credentials=True,
     allow_methods=["*"],
